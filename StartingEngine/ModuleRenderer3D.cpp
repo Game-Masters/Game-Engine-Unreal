6de011dec95cf6a19fdcb79f58316eae6fb18871
Daniel Olondriz Rosique <oldan1997@gmail.com>
2017-09-27 11:36:23 +0200

    Super Amazing cube

--- StartingEngine/ModuleRenderer3D.cpp
+++ StartingEngine/ModuleRenderer3D.cpp
@@ -97,6 +97,7 @@ bool ModuleRenderer3D::Init()
 		lights[0].Active(true);
 		glEnable(GL_LIGHTING);
 		glEnable(GL_COLOR_MATERIAL);
+
 	}
 
 	// Projection matrix for
@@ -121,8 +122,97 @@ update_status ModuleRenderer3D::PreUpdate(float dt)
 	glMatrixMode(GL_MODELVIEW);
 	glLoadMatrixf(App->camera->GetViewMatrix());
 
+	glLineWidth(2.0f);
+	glBegin(GL_TRIANGLES);
+	
+	glVertex3f(0, 1, 0);
+	glVertex3f(0, 0, 0);
+	glVertex3f(1, 0, 0);
+
+	glVertex3f(0, 1, 0);
+	glVertex3f(1, 0, 0);
+	glVertex3f(1, 1, 0);
+
+	glVertex3f(1, 1, 0);
+	glVertex3f(1, 0, 0);
+	glVertex3f(1, 1, -1);
+	
+	glVertex3f(1, 0, 0);
+	glVertex3f(1, 0, -1);
+	glVertex3f(1, 1, -1);
+
+	glVertex3f(0, 1, 0);
+	glVertex3f(1, 1, 0);
+	glVertex3f(1, 1, -1);
+
+	glVertex3f(0, 1, 0);
+	glVertex3f(1, 1, -1);
+	glVertex3f(0, 1,-1);
+
+	glVertex3f(0, 1, 0);
+	glVertex3f(0, 0, -1);
+	glVertex3f(0, 0, 0);
+
+	glVertex3f(0, 1, 0);
+	glVertex3f(0, 1, -1);
+	glVertex3f(0, 0,-1);
+
+	glVertex3f(1, 1, -1);
+	glVertex3f(1, 0, -1);
+	glVertex3f(0, 0, -1);
+
+	glVertex3f(1, 1, -1);
+	glVertex3f(0, 0, -1);
+	glVertex3f(0, 1, -1);
+
+	glVertex3f(0, 0, -0);
+	glVertex3f(0, 0, -1);
+	glVertex3f(1, 0, -1);
+
+	glVertex3f(0, 0, 0);
+	glVertex3f(1, 0, -1);
+	glVertex3f(1, 0, 0);
+	
+	
+	
+	
 	
 
+	
+
+
+	glEnd();
+	glLineWidth(1.0f);
+	/*
+	static const GLfloat g_vertex_buffer_data[] = {
+		0.0f, -1.0f, 0.0f,
+		0.0f, -0.0f, 0.0f,
+		0.0f,  1.0f, 0.0f,
+
+	};
+	GLuint vertexbuffer;
+	// Generate 1 buffer, put the resulting identifier in vertexbuffer
+	glGenBuffers(1, &vertexbuffer);
+	// The following commands will talk about our 'vertexbuffer' buffer
+	glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+	// Give our vertices to OpenGL.
+	glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
+	// 1rst attribute buffer : vertices
+	glEnableVertexAttribArray(0);
+	glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+	glVertexAttribPointer(
+		0,                  // attribute 0. No particular reason for 0, but must match the layout in the shader.
+		3,                  // size
+		GL_FLOAT,           // type
+		GL_FALSE,           // normalized?
+		0,                  // stride
+		(void*)0            // array buffer offset
+	);
+
+	glDrawArrays(GL_TRIANGLES, 0, 3); // Starting from vertex 0; 3 vertices total -> 1 triangle
+	glDisableVertexAttribArray(0);
+	
+	glDeleteBuffers(1, &vertexbuffer);*/
 	// light 0 on cam pos
 	lights[0].SetPos(App->camera->Position.x, App->camera->Position.y, App->camera->Position.z);
 

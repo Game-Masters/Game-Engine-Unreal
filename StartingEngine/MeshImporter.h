1a82641261e01ee09f0401fc600cf130e1f55128
nicobabot <nico.babotaltisen@gmail.com>
2017-10-18 08:44:23 +0200

    FIxed Bug

--- StartingEngine/MeshImporter.h
+++ StartingEngine/MeshImporter.h
@@ -1,10 +1,11 @@
 #pragma once
-#include"Application.h"
+#include"Globals.h"
+#include<vector>
 class MeshImporter
 {
 public:
 	void ImportMesh(const char* name_mesh,std::vector<float>vertex, std::vector<uint> index, std::vector<float>texture_coord, std::vector<float> normals);
 	MeshImporter();
 	~MeshImporter();
-};
+	};
 

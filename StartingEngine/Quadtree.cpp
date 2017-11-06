dc4ffafadbd1c9d639cf4c6b353387fb760fb08e
Daniel Olondriz Rosique <oldan1997@gmail.com>
2017-11-07 00:14:37 +0100

    Continued quadtree

--- StartingEngine/Quadtree.cpp
+++ StartingEngine/Quadtree.cpp
@@ -1,17 +1,33 @@
 #include "Quadtree.h"
-
+#define  U_NE 0
+#define  U_SE 1
+#define  U_SW 2
+#define  U_NW 3
+#define  D_NE 4
+#define  D_SE 5
+#define  D_SW 6
+#define  D_NW 7
 //QUADTREE NODE
 QuadTreeNode::QuadTreeNode()
 {
 }
-
+QuadTreeNode::QuadTreeNode(float3 min_point, float3 max_point, int max_objects) :max_game_objects(max_objects)
+{
+	bounds.maxPoint = max_point;
+	bounds.minPoint + min_point;
+}
 QuadTreeNode::~QuadTreeNode()
 {
 }
 
 void QuadTreeNode::Insert(GameObject * game_object)
 {
+
 	gameobjs.push_back(game_object);
+}
+
+void QuadTreeNode::Erase(GameObject * game_object)
+{
 }
 
 void QuadTreeNode::Clear()
@@ -26,6 +42,28 @@ void QuadTreeNode::Clear()
 
 void QuadTreeNode::Subdivide()
 {
+	float3 center_point = { (bounds.minPoint.x + bounds.maxPoint.x) / 2,0,(bounds.minPoint.z + bounds.maxPoint.z) / 2 };
+	float3 min_point1 = bounds.minPoint;
+	float3 max_point1 = { center_point.x,bounds.maxPoint.y,center_point.z };
+	float3 min_point2 = { center_point.x,bounds.minPoint.y,center_point.z };
+	float3 max_point2 = bounds.maxPoint;
+	float3 min_point3 = { center_point.x,bounds.minPoint.y,bounds.minPoint.z };
+	float3 max_point3 = { bounds.maxPoint.x,bounds.maxPoint.y,center_point.z };
+	float3 min_point4 = { bounds.minPoint.x,bounds.minPoint.y,center_point.z };
+	float3 max_point4 = { center_point.x,bounds.maxPoint.y,bounds.maxPoint.z };
+	QuadTreeNode* child1 = new QuadTreeNode(min_point1, max_point1, NODE_CAPACITY);
+	QuadTreeNode* child2 = new QuadTreeNode(min_point2, max_point2, NODE_CAPACITY);
+	QuadTreeNode* child3 = new QuadTreeNode(min_point3, max_point3, NODE_CAPACITY);
+	QuadTreeNode* child4 = new QuadTreeNode(min_point4, max_point4, NODE_CAPACITY);
+
+	children.push_back(child1);
+	child1->Fill();
+	children.push_back(child2);
+	child2->Fill();
+	children.push_back(child3);
+	child3->Fill();
+	children.push_back(child4);
+	child4->Fill();
 }
 
 void QuadTreeNode::Fill()
@@ -40,6 +78,12 @@ std::vector<QuadTreeNode*> QuadTreeNode::GetChildren()
 std::vector<GameObject*> QuadTreeNode::GetGameObjects()
 {
 	return gameobjs;
+	
+}
+
+bool QuadTreeNode::IsLeaf() const
+{
+	return children[0] == nullptr;
 }
 
 //QUADTREE
@@ -55,6 +99,19 @@ void QuadTreeZ::Calculate()
 {
 	root.Clear();
 	root.Fill();
+	
+}
+
+void QuadTreeZ::SetBoundaries(const AABB & bounds)
+{
+}
+
+void QuadTreeZ::Insert(GameObject * game_object)
+{
+}
+
+void QuadTreeZ::Erase(GameObject * game_object)
+{
 }
 
 void QuadTreeZ::Clear()
--- StartingEngine/Quadtree.h
+++ StartingEngine/Quadtree.h
@@ -1,25 +1,33 @@
 #include"Globals.h"
 #include "Application.h"
-#define NODE_CAPACITY = 4;
+#define NODE_CAPACITY  4
 class GameObject;
 
 class QuadTreeNode
 {
 public:
 	QuadTreeNode();
+	QuadTreeNode(float3 min_point, float3 max_point, int max_objects);
 	~QuadTreeNode();
 	void Insert(GameObject* game_object);
+	void Erase(GameObject* game_object);
 	void Clear();
 	void Subdivide();
 	void Fill();
 	std::vector<QuadTreeNode*> GetChildren();
 	std::vector<GameObject*> GetGameObjects();
+	bool IsLeaf() const;
 
 public:
 
 	std::vector<GameObject*> gameobjs;
 	std::vector<QuadTreeNode*> children;
+	QuadTreeNode* parent = nullptr;
+	AABB bounds;
+	int max_game_objects = NODE_CAPACITY;
 };
+
+
 class QuadTreeZ
 {
 public:
@@ -27,6 +35,9 @@ public:
 	QuadTreeZ();
 	~QuadTreeZ();
 	void Calculate();
+	void SetBoundaries(const AABB& bounds);
+	void Insert(GameObject*  game_object);
+	void Erase(GameObject*  game_object);
 	void Clear();
 	QuadTreeNode root;
 	std::vector<GameObject*> gameobjs;
